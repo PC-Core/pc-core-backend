689b38701cea91d56e676a7bc50f0ecc2ec5ac5f
Alv1ol <alv1olgg@gmail.com>
2025-10-06 15:59:45 +0300

    Add Seeds

--- pkg/models/keyboardChars.go
+++ pkg/models/keyboardChars.go
@@ -4,11 +4,11 @@ type KeyboardChars struct {
 	ID            uint64   `json:"id"`
 	Name          string   `json:"name"`
 	TypeKeyBoards string   `json:"type_keyboards"`
-	Switches      []string `json:"switches"`
+	Switches      string `json:"switches"`
 	ReleaseYear   uint64   `json:"release_year"`
 }
 
-func NewKeyBoardChars(id uint64, name, type_keyboards string, switches []string, release_year uint64) *KeyboardChars{
+func NewKeyBoardChars(id uint64, name, type_keyboards string, switches string, release_year uint64) *KeyboardChars{
 	return &KeyboardChars{
 		id, name, type_keyboards, switches, release_year,
 	}
